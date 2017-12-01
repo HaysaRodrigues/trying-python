5833447af033e4a7d5a191f4d22b349960736f22
Haysa <haysa.rodrigues@liferay.com>
2017-12-01 16:46:10 -0300

    read and manipulating strings

---
+++ applications/__myNameAndAge__.py
@@ -0,0 +1,16 @@
+print('Hi there!')
+print('What is your name?')
+
+myName = input()
+
+print('So nice to know you' + myName)
+print('The length of your name is: ')
+print(len(myName))
+
+print('How old are you?')
+
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1) + ' next year.')
+
+
--- introduction/ManipulatingValues.py
+++ introduction/ManipulatingValues.py
@@ -1,3 +1,30 @@
 # Concat strings
 
 print('lo' + 've')
+
+# Keep values in a var
+
+spam = 40
+print(spam)
+
+# concat the var
+foo = 'love'
+mix = 'strings'
+
+print(foo + mix)
+
+# changing the value of foo
+foo = 'bar'
+print(foo)
+
+# ---------------------------------------- #
+# https://www.python.org/dev/peps/pep-0008/
+# ---------------------------------------- #
+
+# In python you can use the operator * with strings
+
+foo = 'bar'
+print(foo * 10)
+
+
+
