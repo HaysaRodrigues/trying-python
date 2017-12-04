9a684e60d19048817b13ce415071fbdee08c7071
Haysa <haysa.rodrigues@liferay.com>
2017-12-04 17:45:58 -0300

    more python data manipulation with str, int, float, round

--- applications/__myNameAndAge__.py
+++ applications/__myNameAndAge__.py
@@ -1,10 +1,13 @@
 print('Hi there!')
 print('What is your name?')
 
+# read input
 myName = input()
 
 print('So nice to know you' + myName)
 print('The length of your name is: ')
+
+# len(myString) tells the length of the string
 print(len(myName))
 
 print('How old are you?')
@@ -12,5 +15,4 @@ print('How old are you?')
 myAge = input()
 
 print('You will be ' + str(int(myAge) + 1) + ' next year.')
-
 
--- introduction/ManipulatingValues.py
+++ introduction/ManipulatingValues.py
@@ -26,5 +26,28 @@ print(foo)
 foo = 'bar'
 print(foo * 10)
 
+# in python if you want to concat a string with a integer you have to:
+
+print(str(29) + ' <-- This is a string with an integer.')
+print(float(3.99))
+print(int(-99))
+
+# sum up two integer
+print(int(8) + 87)
+
+# if I want to get some value from user and do some math operation
+
+print('Tell me the number: ')
+myInt = input()
+
+myInt = int(myInt)
+
+sec = myInt + 2
+
+print(sec)
+
+# to round up some value
+print(round(5.8787878787, 2))
+
 
 
